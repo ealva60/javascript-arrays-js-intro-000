ec17a517cb379ef1beba20be29bbbaad079a46fe
Erick A <servinfma@gmail.com>
2021-01-29 05:10:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array) {
+  var narray = array.slice(1)
+  return narray
+}
