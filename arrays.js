d16da420264a5738d5ffe9cd021f5aeabfcd184c
Erick A <servinfma@gmail.com>
2021-01-29 05:18:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  var narray = array.pop()
+  var narray = array.slice(0, array.length - 1)
   return narray
 }
